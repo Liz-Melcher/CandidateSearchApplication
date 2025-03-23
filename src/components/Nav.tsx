2e201c4910793e617337b36f5cf13e16197f9ff8
Liz Melcher <lizmelchercodingbootcamp@gmail.com>
2025-03-22 21:20:21 -0500

    updated nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,37 @@
+// TODO: Add necessary code to display the navigation bar and link between the pages
+
+import { Link, useLocation} from 'react-router-dom'
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
+  const location= useLocation()
+
+  
   return (
-    <div>Nav</div>
+    <nav>
+    <ul>
+      <li>
+        <Link
+          to="/"
+          className={
+            location.pathname === '/' 
+            ? 'nav-link active' 
+            : 'nav-link'}
+        >
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link 
+        to= "SavedCandidates"
+        className={
+          location.pathname === '/SavedCandidates'
+          ? 'nav-link active'
+        }
+        > Potential Candidates
+        </Link>
+      </li>
+    </ul>
+  </nav>
   )
 };
 
